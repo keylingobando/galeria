011265e09e00f31c5068a6e66bdf2a1028c2e0bf
keyling <keyling.obandofonseca@tiernoparla.es>
2023-01-23 18:11:49 +0100

    commit inicial practica1

---
+++ src/main/java/programacion/galeria/Galeria.java
@@ -0,0 +1,13 @@
+package programacion.galeria;
+
+/**
+ * Hello world!
+ *
+ */
+public class Galeria 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
